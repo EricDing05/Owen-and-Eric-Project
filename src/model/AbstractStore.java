1a6537746b7916c6655b5fdc823df51d779deb12
owenfisher <owenfisher46@gmail.com>
2024-05-09 20:42:45 -0700

    - put some stuff in the abstract store

---
+++ src/model/AbstractStore.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.*;
+
+public abstract class AbstractStore {
+
+    private String name;
+    protected List<Product> products;
+    protected Scraper scraper;
+
+    public AbstractStore(String name) {
+        this.name = name;
+    }
+
+    public void addProduct(Product p) {
+        this.products.add(p);
+    }
+
+    public abstract void generateProducts();
+
+
+}
