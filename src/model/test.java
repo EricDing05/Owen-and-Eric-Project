461120f461a79ffe5dfb3d1be38500f3a318fd9c
ericding <ericding555@gmail.com>
2024-06-10 19:16:44 -0700

    added safari

--- src/model/test.java
+++ src/model/test.java
@@ -1,12 +1,15 @@
 package model;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 
 public class test {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new SafariDriver();
+
+        driver.get("https://www.walmart.ca/en/shop/weekly-flyer-features/6000196190101?catId=10019&icid=cp_l1_page_grocery_savings_weekly_flyer_56871_LY6XMFKK3F");
 
     }
 
