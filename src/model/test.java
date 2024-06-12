0a7b8424e27f3601c0cf3a80807f105272d0716f
owenfisher <owenfisher46@gmail.com>
2024-06-11 20:45:34 -0700

    - here is a elementary working scraper for a walmart page.

--- src/model/test.java
+++ src/model/test.java
@@ -1,7 +1,11 @@
 package model;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.List;
 
 
 public class test {
@@ -9,7 +13,24 @@ public class test {
     public static void main(String[] args) {
         WebDriver driver = new SafariDriver();
 
-        driver.get("https://www.walmart.ca/en/shop/weekly-flyer-features/6000196190101?catId=10019&icid=cp_l1_page_grocery_savings_weekly_flyer_56871_LY6XMFKK3F");
+        // driver.get("https://www.walmart.ca/en/ip/strawberries/871284"); // single item test
+        // WebElement priceElement = driver.findElement(By.cssSelector("span[itemprop='price']")); // goes with that ^
+        driver.get("https://www.walmart.ca/en/browse/grocery/fruits-vegetables/fresh-fruits/10019_6000194327370_6000194327411?icid=cp_l2_page_grocery_fresh_fruits_22956_QV0B69TN6Z"); // multiple items on a page
+        //WebElement priceElement = driver.findElement(By.cssSelector("div[data-automation-id='product-price'] .mr1.mr2-xl.b.black.lh-copy.f5.f4-l"));
+
+        //this gets all the product prices on a given walmart grocery item page.
+        List<WebElement> priceElements = driver.findElements(By.cssSelector("div[data-automation-id='product-price'] .mr1.mr2-xl.b.black.lh-copy.f5.f4-l"));
+
+        //just prints the price (can edit so prints out name and stuff)
+        for (WebElement priceElement : priceElements) {
+            String price = priceElement.getText();
+            System.out.println("Price: " + price);
+        }
+
+
+
+        // Close the browser
+        driver.quit();
 
     }
 
