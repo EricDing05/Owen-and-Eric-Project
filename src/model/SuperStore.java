69c629706697f1030d53d51d56dd036e94b71e1f
owenfisher <owenfisher46@gmail.com>
2024-06-13 15:58:13 -0700

    - added a wait time to load the page. which helps pre vent "No such element exception" by ensuring that the page is loaded before the page is scraped. Also the selenium has to be changed to selenium.java.4.XX to use this feature. this version is more general and has more functionality.

--- src/model/SuperStore.java
+++ src/model/SuperStore.java
@@ -4,46 +4,51 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class SuperStore {
-
     public static void main(String[] args) {
         WebDriver driver = new SafariDriver();
-        driver.get("https://www.realcanadiansuperstore.ca/food/fruits-vegetables/fresh-vegetables/c/28195?navid=flyout-L3-Fresh-Vegetables");
 
-//        WebElement priceContainer = driver.findElement(By.xpath("//p[@class='chakra-text css-o93gbd' and @data-testid='mop-regular-price']"));
-//        WebElement priceElement = priceContainer.findElement(By.tagName("span"));
-//        String priceText = priceElement.getText();
-//        System.out.println("Price: " + priceText);
+        try {
+            driver.get("https://www.realcanadiansuperstore.ca/food/fruits-vegetables/fresh-vegetables/c/28195?navid=flyout-L3-Fresh-Vegetables");
 
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        //Super store vegetables
-        WebElement gridElement = driver.findElement(By.xpath("//div[@data-testid='product-grid']"));
+            WebElement gridElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-testid='product-grid']")));
+            // Super store vegetables
 
-        List<WebElement> productTiles = gridElement.findElements(By.xpath(".//div[@data-testid='price-product-tile']"));
+            List<WebElement> productTiles = gridElement.findElements(By.xpath(".//div[@data-testid='price-product-tile']"));
 
-        //should wrap this for loop inside another loop that loops throuhg the page index based off the number of pages we can find
-        for (WebElement productTile : productTiles) {
-            try {
-                // Find the span element with data-testid="sale-price" or data-testid="regular-price" within the product tile
-                WebElement priceElement = productTile.findElement(By.xpath(".//span[@data-testid='sale-price' or @data-testid='regular-price']"));
-                String priceText = priceElement.getText();
-                System.out.println("Price: " + priceText);
-            } catch (Exception e) {
-                System.out.println("Price element not found in this product tile.");
+            // should wrap this for loop inside another loop that loops through the page index based off the number of pages we can find
+            for (WebElement productTile : productTiles) {
+                try {
+                    // Find the span element with data-testid="sale-price" or data-testid="regular-price" within the product tile
+                    WebElement priceElement = productTile.findElement(By.xpath(".//span[@data-testid='sale-price' or @data-testid='regular-price']"));
+                    String priceText = priceElement.getText();
+                    System.out.println("Price: " + priceText);
+                } catch (Exception e) {
+                    System.out.println("Price element not found in this product tile.");
+                }
+
+                // Add a delay of 2 seconds (2000 milliseconds)
+                try {
+                    Thread.sleep(2000); // should make this random to avoid detection
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
 
-            // Add a delay of 2 seconds (2000 milliseconds)
-            try {
-                Thread.sleep(2000); // should make this random to avoid detection
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        } catch (Exception ex) {
+            System.out.println("An error occurred: " + ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            // Close the browser
+            driver.quit();
         }
-        // Close the browser
-        driver.quit();
     }
-
 }
