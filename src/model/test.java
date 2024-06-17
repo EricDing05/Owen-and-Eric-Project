8d0ce8c666a481190fa5fed26177f978fa7583bd
ericding <ericding555@gmail.com>
2024-06-17 15:15:47 -0700

    small updates

--- src/model/test.java
+++ src/model/test.java
@@ -1,15 +1,43 @@
 package model;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+import java.util.List;
 
 
 public class test {
 
     public static void main(String[] args) {
+
         WebDriver driver = new SafariDriver();
 
-        driver.get("https://www.walmart.ca/en/shop/weekly-flyer-features/6000196190101?catId=10019&icid=cp_l1_page_grocery_savings_weekly_flyer_56871_LY6XMFKK3F");
+        try {
+
+            driver.get("https://www.realcanadiansuperstore.ca/");
+
+            System.out.println(driver.getTitle());
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+
+            extractProductInfo(driver);
+
+
+        } catch (Exception e) {
+            driver.quit();
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void extractProductInfo(WebDriver driver) {
+        // Find the product elements using data-testid attributes
+        List<WebElement> products = driver.findElements(By.className("css-0"));
+        System.out.println(products.size());
 
     }
 
