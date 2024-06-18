9892a79bffcff6898211825b8cd67f5dcf7c1419
owenfisher <owenfisher46@gmail.com>
2024-06-17 19:08:36 -0700

    - made store package

---
+++ src/model/store/WebsiteScraper.java
@@ -0,0 +1,17 @@
+package model.store;
+
+public abstract class WebsiteScraper {
+
+
+    public void scrapeWebsite() {
+
+    }
+
+    public void scrapeCategory() {
+
+    }
+
+    public abstract void scrapePage();
+
+
+}
