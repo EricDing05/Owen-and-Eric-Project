4f4cdec7d4a909e9ba365b34499ee62d4a89678f
owenfisher <owenfisher46@gmail.com>
2024-06-17 19:18:18 -0700

    - abstract store class

--- src/model/AbstractStore.java
+++ src/model/AbstractStore.java
@@ -1,19 +1,21 @@
 package model;
 
+import model.store.WebsiteScraper;
+
 import java.util.*;
 
 public abstract class AbstractStore {
 
     private String name;
     protected List<Product> products;
-    protected Scraper scraper;
+    protected WebsiteScraper scraper;
+    private String gridPath;
+    private String productPath;
+    private String infoPath;
+
 
     public AbstractStore(String name) {
         this.name = name;
-    }
-
-    public void addProduct(Product p) {
-        this.products.add(p);
     }
 
     public abstract void generateProducts();
