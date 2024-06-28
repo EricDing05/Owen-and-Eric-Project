eb6e25fcd7019c1202f9b61196cffd298c9a180d
owenfisher <owenfisher46@gmail.com>
2024-06-27 19:41:53 -0700

    - scrape page abstracted, walmart getting blocked, made new noproducts exceptoin.

--- src/model/test.java
+++ src/model/test.java
@@ -1,14 +1,27 @@
 package model;
 
 
-import model.store.Walmart;
+import model.store.Costco;
+import model.store.ThriftyFoods;
+import model.store.*;
 
 public class test {
 
     public static void main(String[] args) {
-        AbstractStore Costco = new Walmart("costco");  //save on, thriftys
-        System.out.println(Costco.getCategoriesURLs().values());
+        AbstractStore Costco = new Costco("costco");  //save on, thriftys
         Costco.generateProducts();
+        AbstractStore Thriftyfoods = new ThriftyFoods("Thriftyfoods");  //save on, thriftys
+        Thriftyfoods.generateProducts();
+        AbstractStore SaveOnFoods = new SaveOnFoods("SaveOnFoods");  //save on, thriftys
+
+        SaveOnFoods.generateProducts();
+        Costco.generateProducts();
+        Thriftyfoods.generateProducts();
+
+        Costco.saveProducts();
+        Thriftyfoods.saveProducts();
+        SaveOnFoods.saveProducts();
+
     }
 
 }
