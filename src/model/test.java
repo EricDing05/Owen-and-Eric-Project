98c431882210d367cad7391ed6a9d787f34571f0
owenfisher <owenfisher46@gmail.com>
2024-06-30 18:52:52 -0700

    000

--- src/model/test.java
+++ src/model/test.java
@@ -2,12 +2,12 @@ package model;
 
 
 import model.store.AbstractStore;
-import model.store.ThriftyFoods;
+import model.store.NoFrills;
 
 public class test {
 
     public static void main(String[] args) {
-        AbstractStore Costco = new ThriftyFoods("costco");  //save on, thriftys
+        AbstractStore Costco = new NoFrills("costco");  //save on, thriftys
         System.out.println(Costco.getCategoriesURLs().values());
         Costco.generateProducts();
         Costco.save();
