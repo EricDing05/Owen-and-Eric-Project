1c1d183b9cc1c785b8aeec7a62001fa6a00892e6
ericding <ericding555@gmail.com>
2024-06-30 19:02:29 -0700

    small changes

--- src/model/test.java
+++ src/model/test.java
@@ -2,12 +2,12 @@ package model;
 
 
 import model.store.AbstractStore;
-import model.store.SuperStore;
+import model.store.Costco;
 
 public class test {
 
     public static void main(String[] args) {
-        AbstractStore Costco = new SuperStore("costco");  //save on, thriftys
+        AbstractStore Costco = new Costco("costco");  //save on, thriftys
         System.out.println(Costco.getCategoriesURLs().values());
         Costco.generateProducts();
         Costco.save();
