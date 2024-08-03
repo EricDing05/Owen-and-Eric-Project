d311b705130a4d8b1c39aded5f08a5b651fcccec
ericding <ericding555@gmail.com>
2024-08-03 14:57:02 -0700

    made home look better

--- xtreme-react-lite-master/package/src/views/ui/Home.js
+++ xtreme-react-lite-master/package/src/views/ui/Home.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col, Button, Form, FormGroup, Label, Input, ListGroup, ListGroupItem } from 'reactstrap';
+
 const Home = () => {
     return (
-        <div className="home-container">
-            <header className="home-header">
-                <h1>Welcome to Grocery Guru</h1>
-            </header>
-            <main className="home-main">
-                <p>Grocery Guru helps you find the best prices for your grocery list across different stores.
-                Simply create a list, and we'll show you the price of that list at every store.</p>
-                <Link to="/newlist" className="get-started-button">
-                    Get Started
-                </Link>
-            </main>
-        </div>
+        <Container className="pageWrapper home-container">
+            <Row>
+                <Col>
+                    <header className="home-header">
+                        <h1>Welcome to Grocery Guru</h1>
+                    </header>
+                </Col>
+            </Row>
+            <Row className="contentArea">
+                <Col>
+                    <main className="home-main">
+                        <p>Grocery Guru helps you find the best prices for your grocery list across different stores.
+                        Simply create a list, and we'll show you the price of that list at every store.</p>
+                        <Link to="/newlist">
+                            <Button color="primary">Get Started</Button>
+                        </Link>
+                    </main>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
