8ba3f95fa5bf4077ffef768ca80fb1fe2943b40b
ericding <ericding555@gmail.com>
2024-08-03 14:50:45 -0700

    added a "get started" button on the home page

--- xtreme-react-lite-master/package/src/views/ui/Home.js
+++ xtreme-react-lite-master/package/src/views/ui/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Row, Col, Button, Form, FormGroup, Label, Input, ListGroup, ListGroupItem } from 'reactstrap';
-
 const Home = () => {
     return (
         <div className="home-container">
@@ -10,6 +10,9 @@ const Home = () => {
             <main className="home-main">
                 <p>Grocery Guru helps you find the best prices for your grocery list across different stores.
                 Simply create a list, and we'll show you the price of that list at every store.</p>
+                <Link to="/newlist" className="get-started-button">
+                    Get Started
+                </Link>
             </main>
         </div>
     );
